467b4763885c62bcf966456bc7329f107bc3bd2b
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-07-18 08:18:54 -0300

    basic serialization

---
+++ app/serializers/todo.js
@@ -0,0 +1,28 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+
+  normalizeResponse(store, primaryModelClass, payload) {
+      let normalizedPayload = { data: [] };
+
+      if (Array.isArray(payload)) { // a list
+        payload.forEach(todoJSON => {
+          normalizedPayload.data.push(this.normalizeTodo(todoJSON));
+        })
+      } else { // one element
+        normalizedPayload.data = this.normalizeTodo(payload);
+      }
+
+      return normalizedPayload;
+  },
+
+  normalizeTodo(todoJSON) {
+    return {
+      id: todoJSON.id,
+      type: 'todo',
+      attributes: {
+        description: todoJSON.description
+      }
+    }
+  }
+});
